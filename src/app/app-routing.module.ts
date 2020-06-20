83f458b2a675aca58bdbd07565f1aa1a50268782
Mussuranae <mandonnetdev@outlook.com>
2020-06-20 20:24:57 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { DisplayComponent } from './characters/display/display.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: DisplayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
